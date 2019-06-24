d7950d8c720145568fde4bcd9693395ae84c4908
MrKevinWeiss <weiss.kevin604@gmail.com>
2019-06-24 12:14:12 +0200

    refactor(mem_map*): Remove all mem map features
    Since philip_pal is now in the PHiLIP repo it can be removed from riot_pal
    philip_pal can now be installed with pip3 install philip_pal

--- riot_pal/__init__.py
+++ riot_pal/__init__.py
@@ -6,11 +6,6 @@
 """packet init for RIOT PAL
 This exposes usful modules in the RIOT PAL packet
 """
-from pathlib import Path
 from .dut_shell import DutShell
-from .ll_shell import LLShell
-from .ll_mem_map_if import LLMemMapIf
 
-PHILIP_MEM_MAP_PATH = str(Path(__file__).parents[0]) + \
-                      '/mem_map/philip_mem_map-00_00_01.csv'
-__all__ = ['DutShell', 'LLShell', 'LLMemMapIf']
+__all__ = ['DutShell']
