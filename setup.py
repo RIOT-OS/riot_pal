5b117990d423b23dc4a181deff276e642df9386f
MrKevinWeiss <weiss.kevin604@gmail.com>
2019-08-28 14:05:16 +0200

    fix(riot_pal): Handle timeout exception
    Make riot_driver throw timeout execption
    Catch timeout exception in dut_shell

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="riot_pal",
-    version="0.3.1",
+    version="0.3.2",
     author="RIOT OS",
     author_email="devel@riot-os.org",
     license="MIT",
